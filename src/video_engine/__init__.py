c175bd5291d1c54fe7b03b0cd9991e54a4fab636
Code-Krasher09 <kumarsamarth272@gmail.com>
2026-06-16 23:48:52 +0530

    feat(video-engine): add audio processing support and configure pytest/pyright

--- src/video_engine/__init__.py
+++ src/video_engine/__init__.py
@@ -1,7 +1,8 @@
 """Video engine package for Theia Video Enhancer."""
 
+from .audio_manager import AudioManager, AudioProcessingError
 from .frame_extractor import FrameExtractor
 from .video_reader import VideoReader
 from .video_writer import VideoWriter
 
-__all__ = ["FrameExtractor", "VideoReader", "VideoWriter"]
+__all__ = ["AudioManager", "AudioProcessingError", "FrameExtractor", "VideoReader", "VideoWriter"]
