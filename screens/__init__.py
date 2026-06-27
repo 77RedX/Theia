3f3dd1f2ba52d153eda3f26193fc0d11ae17f050
77RedX <akshaygautam4450@gmail.com>
2026-06-27 17:45:07 +0530

    Cleaned main branch

--- screens/__init__.py
+++
@@ -1,5 +0,0 @@
-# Theia screens package
-from screens.home_screen import HomeScreen
-from screens.settings_screen import SettingsScreen
-from screens.processing_screen import ProcessingScreen
-from screens.results_screen import ResultsScreen
