5e2b40010e0c9aaf1c1994df40c16172200cfae5
Jayesh <jayeshmjk24@gmail.com>
2026-07-31 00:51:01 +0530

    Added .exe file

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import os
 sys.path.insert(0, os.path.abspath("src"))
 
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 from app import TheiaApp
 from controllers.application_controller import ApplicationController
+from pathlib import Path
 
 def main():
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(str(Path("assets/icon.jpg"))))
     window = TheiaApp()
     controller = ApplicationController(window)
     window.show()
